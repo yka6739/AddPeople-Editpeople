d8cc5808334c70e15f1b51dbe998fe026b491332
YATENDRA AWANA <yka6739@gmail.com>
2021-08-15 20:09:51 +0530

    Add project files.

---
+++ WebApplication14/Shared/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication14.Shared
+{
+   public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public string Picture { get; set; }
+       [Required]
+        public DateTime? DateofBirth { get; set; }
+    }
+}
